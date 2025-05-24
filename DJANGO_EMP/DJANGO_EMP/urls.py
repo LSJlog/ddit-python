568a7b0772c628ff1d763857907b16e37c202e31
sujin <sjlee_n@naver.com>
2025-05-24 20:24:41 +0900

    Django

--- DJANGO_EMP/DJANGO_EMP/urls.py
+++ DJANGO_EMP/DJANGO_EMP/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from DJANGO_EMP import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('emp_list', views.emp_list),
+    path('', views.emp_List),
+    path('emp_list', views.emp_List),
+    path('emp_detail', views.emp_detail),
+    #path('emp_detail/', views.emp_detail),
+    path('emp_mod', views.emp_mod),
+    path('emp_mod_act', views.emp_mod_act),
+    path('emp_add', views.emp_add),
+    path('emp_add_act', views.emp_add_act),
+    path('emp_del_act', views.emp_del_act),
 ]
